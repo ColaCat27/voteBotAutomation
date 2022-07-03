e29d3d59c0b3711c8de51488589c974e61c5e794
yo <colacat1927@gmail.com>
2022-07-04 01:37:57 +0300

    добавил первоначальные настройки и модули

--- bot.js
+++ bot.js
@@ -2,24 +2,37 @@ const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 
+require("dotenv").config();
+
 puppeteer.use(StealthPlugin());
-puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
+puppeteer.use(
+  AdblockerPlugin({
+    blockTrackers: true,
+  })
+);
 
-// That's it, the rest is puppeteer usage as normal 😊
-puppeteer.launch({ headless: true }).then(async (browser) => {
-  const page = await browser.newPage();
-  await page.setViewport({ width: 800, height: 600 });
+const settings = {
+  threads: parseInt(process.env.THREADS),
+  proxyPath: process.env.PROXY_PATH,
+  targetLink: process.env.TARGET_SITE,
+};
 
-  console.log(`Testing adblocker plugin..`);
-  await page.goto("https://www.vanityfair.com");
-  await page.waitForTimeout(1000);
-  await page.screenshot({ path: "adblocker.png", fullPage: true });
+puppeteer
+  .launch({
+    headless: false,
+    args: ["--start-maximized"],
+    ignoreDefaultArgs: ["--enable-automation"],
+    waitUntil: "networkidle2",
+  })
+  .then(async (browser) => {
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
 
-  console.log(`Testing the stealth plugin..`);
-  await page.goto("https://bot.sannysoft.com");
-  await page.waitForTimeout(5000);
-  await page.screenshot({ path: "stealth.png", fullPage: true });
-
-  console.log(`All done, check the screenshots. ✨`);
-  await browser.close();
-});
+    await page.goto(settings.targetLink);
+    await page.waitForSelector("#captcha", { visible: true }).then(() => {
+      console.log("Captcha finded");
+    });
+    const captcha = await page.$eval("#captcha", (el) => el.src);
+    console.log(`Captcha link: ${captcha}`);
+    await browser.close();
+  });
