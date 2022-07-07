2139ad710df7822c251bd8a0d1d8e76a3ca87959
yo <colacat1927@gmail.com>
2022-07-07 12:51:38 +0300

    добавил многопоток и настройки через модуль input

--- bot.js
+++ bot.js
@@ -1,12 +1,9 @@
+const { Cluster } = require("puppeteer-cluster");
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 const { firstPoint } = require("./points/firstPoint");
-const input = require("input");
-const os = require("os");
-const fs = require("fs");
-
-const cpuLength = os.cpus().length; //узнаем количество ядер
+const { initSettings } = require("./settings/settings");
 
 require("dotenv").config();
 
@@ -35,128 +32,78 @@ puppeteer.use(
   })
 );
 
-const main = async () => {
+(async () => {
   //Set settings
-  const settings = {};
-  settings.threads = await input.text("Set threads count", {
-    validate(answer) {
-      let value = parseInt(answer);
-      if (!Boolean(value)) {
-        console.log(` Укажите число > 0 и < ${cpuLength}, а не строку`);
-      }
-      if (value === 0) {
-        console.log(` Количество потоков должно быть > 0 и < ${cpuLength}`);
-      }
-      if (value > cpuLength) {
-        console.log(
-          ` Вам доступно не больше ${cpuLength} потоков, введите число не больше ${cpuLength}`
-        );
-      }
+
+  const settings = await initSettings();
+
+  //Cluster
+
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: settings.threads,
+    puppeteerOptions: {
+      headless: false,
+      ignoreDefaultArgs: ["--enable-automation"],
     },
   });
 
-  settings.proxyPath = await input.text("Set proxy path", {
-    validate(answer) {
-      if (!fs.existsSync(answer)) {
-        console.log("Укажите правильный путь, файл с прокси не найден");
-      }
-      if (!answer.length) {
-        console.log("Укажите путь к файлу с прокси");
-      }
-    },
+  await cluster.task(async ({ page, data: url }) => {
+    const page = await browser.newPage();
+    await page.goto(settings.targetLink);
+    await firstPoint(page, settings);
+    let isExist;
+    try {
+      isExist = await page.waitForXPath(
+        "//span[contains(text(), 'Please try again!')]",
+        {
+          //поиск ошибки решения первой капчи
+          timeout: 4500,
+        }
+      );
+    } catch {}
+    if (isExist) {
+      await firstPoint(page, settings);
+      return;
+    }
+    try {
+      isExist = await page.waitForXPath("//div[@id='cf-hcaptcha-container']", {
+        timeout: 3000,
+      });
+    } catch {}
+    if (isExist) {
+      console.log("Начинаю разгадывание hcaptcha");
+      await page.solveRecaptchas();
+      console.log("Капча решена");
+      await page.waitForNavigation();
+      // const frame = await page.mainFrame().childFrames()[1]; //(Способ взаимодействовать с фреймом)
+      // await Promise.all([page.waitForNavigation(), frame.click(`#checkbox`)]);
+    }
+    let button;
+    try {
+      button = await page.waitForXPath("//div[@id='cf-norobot-container']");
+    } catch {}
+    if (button) {
+      await button.click();
+    }
+    try {
+      isExist = await page.waitForXPath("//div[@id='middleba']", {
+        timeout: 3000,
+        visible: true,
+      });
+    } catch {}
+    if (!isExist) {
+      console.log("Не удалось проголосовать пытаюсь еще раз");
+      await page.goto(settings.targetLink);
+      await firstPoint(page, settings);
+      return;
+    } else {
+      console.log("Успешно проголосовал за сервер");
+      await browser.close();
+    }
   });
 
-  settings.targetLink = await input.text("Set target link", {
-    validate(answer) {
-      if (!/http(s|):\/\/(www\.|).*(?=\.)/gm.test(answer)) {
-        console.log("Укажите настоящую ссылку");
-      }
-      if (!answer.length) {
-        console.log("Укажите ссылку");
-      }
-    },
-  });
-
-  settings.apiKey = await input.text("Set capmonster api key", {
-    validate(answer) {
-      if (!answer.length) {
-        console.log("Укажите Capmonster API KEY");
-      }
-    },
-  });
-
-  //----------------------
-  // puppeteer
-  //   .launch({ headless: false, ignoreDefaultArgs: ["--enable-automation"] })
-  //   .then(async (browser) => {
-  //     const page = await browser.newPage();
-
-  //     //const frame = await page.mainFrame().childFrames()[1]; (Способ взаимодействовать с фреймом)
-
-  //     await page.goto(settings.targetLink);
-
-  //     await firstPoint(page, settings);
-  //     let isExist;
-
-  //     try {
-  //       isExist = await page.waitForXPath(
-  //         "//span[contains(text(), 'Please try again!')]",
-  //         {
-  //           //поиск ошибки решения первой капчи
-  //           timeout: 4500,
-  //         }
-  //       );
-  //     } catch {}
-
-  //     if (isExist) {
-  //       await firstPoint(page, settings);
-  //       return;
-  //     }
-
-  //     try {
-  //       isExist = await page.waitForXPath("//div[@id='cf-hcaptcha-container']", {
-  //         timeout: 3000,
-  //       });
-  //     } catch {}
-
-  //     if (isExist) {
-  //       console.log("Начинаю разгадывание hcaptcha");
-
-  //       await page.solveRecaptchas();
-  //       console.log("Капча решена");
-  //       await page.waitForNavigation();
-  //       // const frame = await page.mainFrame().childFrames()[1]; //(Способ взаимодействовать с фреймом)
-  //       // await Promise.all([page.waitForNavigation(), frame.click(`#checkbox`)]);
-  //     }
-  //     let button;
-  //     try {
-  //       button = await page.waitForXPath("//div[@id='cf-norobot-container']");
-  //     } catch {}
-
-  //     if (button) {
-  //       await button.click();
-  //     }
-
-  //     try {
-  //       isExist = await page.waitForXPath("//div[@id='middleba']", {
-  //         timeout: 3000,
-  //         visible: true,
-  //       });
-  //     } catch {}
-
-  //     if (!isExist) {
-  //       console.log("Не удалось проголосовать пытаюсь еще раз");
-  //       await page.goto(settings.targetLink);
-
-  //       await firstPoint(page, settings);
-  //       return;
-  //     } else {
-  //       console.log("Успешно проголосовал за сервер");
-  //       await browser.close();
-  //     }
-  //     // await Promise.all([page.waitForNavigation(), page.click(".btn-install")]);
-  //     // -------------------------------------------------------------------------
-  //   });
-};
-
-main();
+  for (let i = 0; i < settings.executions; i++) {
+    cluster.queue("http://www.google.com/");
+  }
+})();
---
+++ settings/settings.js
@@ -0,0 +1,64 @@
+const input = require("input");
+
+module.exports.initSettings = async () => {
+  const settings = {};
+  settings.threads = await input.text("Количество потоков", {
+    validate(answer) {
+      let value = parseInt(answer);
+      if (!Boolean(value)) {
+        console.log(` Укажите число > 0, а не строку`);
+      }
+      if (value === 0) {
+        console.log(` Количество потоков должно быть > 0`);
+      }
+      return true;
+    },
+  });
+
+  settings.executions = await input.text("Количество выполнений", {
+    validate(answer) {
+      let value = parseInt(answer);
+      if (!Boolean(value)) {
+        console.log(` Укажите число > 0, а не строку`);
+      }
+      if (value === 0) {
+        console.log(` Количество выполнений должно быть > 0`);
+      }
+      return true;
+    },
+  });
+
+  settings.proxyPath = await input.text("Путь к файлу с прокси", {
+    validate(answer) {
+      if (!fs.existsSync(answer)) {
+        console.log(" Укажите правильный путь, файл с прокси не найден");
+      }
+      if (!answer.length) {
+        console.log(" Укажите путь к файлу с прокси");
+      }
+      return true;
+    },
+  });
+
+  settings.targetLink = await input.text("Ссылка на сайт", {
+    validate(answer) {
+      if (!/http(s|):\/\/(www\.|).*(?=\.)/gm.test(answer)) {
+        console.log(" Укажите настоящую ссылку");
+      }
+      if (!answer.length) {
+        console.log(" Укажите ссылку");
+      }
+      return true;
+    },
+  });
+
+  settings.apiKey = await input.text("API ключ для капчи", {
+    validate(answer) {
+      if (!answer.length) {
+        console.log(" Укажите Capmonster API KEY");
+      }
+      return true;
+    },
+  });
+  return settings;
+};
